6fa13b0ec89cd6296d98327935be5313a94ad733
tvoutsos <tvoutsos@chicagobooth.edu>
2020-10-13 19:17:21 +0000

    Square totally working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   
       render({ :template => "calculation_templates/square_form.html.erb" })
   end
+
+  def calculate_square
+    # params = {"elephant" =>"42"}
+
+    @num = params.fetch("elephant").to_f
+    @square_of_num = @num * @num
+
+    render({ :template => "calculation_templates/square_results.html.erb" })
+  end
+
 end
